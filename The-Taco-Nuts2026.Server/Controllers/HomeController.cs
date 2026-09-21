29f5f78a041373e2cde82e473b7ce639397aafe5
Brett <brett.karbowski@yahoo.com>
2026-09-21 13:57:12 -0500

    Add project files.

---
+++ The-Taco-Nuts2026.Server/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace The_Taco_Nuts2026.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+    }
+}
---
+++ the-taco-nuts2026.client/src/App.tsx
@@ -0,0 +1,33 @@
+﻿import { useEffect, useState } from 'react';
+import './App.css';
+
+interface Forecast {
+    date: string;
+    temperatureC: number;
+    temperatureF: number;
+    summary: string;
+}
+
+function App() {
+    return (
+        <div className="app">
+            <header className="app-header">
+                <h1>The Taco Nuts</h1>
+                <p>Welcome to our React + ASP.NET Core application!</p>
+            </header>
+
+            <main className="home-content">
+                <h2>Home</h2>
+                <p>
+                    This is the homepage for our project.
+                </p>
+
+                <button onClick={() => alert('Hello from React!')}>
+                    Click me!
+                </button>
+            </main>
+        </div>
+    );
+}
+
+export default App;
